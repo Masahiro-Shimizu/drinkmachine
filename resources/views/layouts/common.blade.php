82a9f347bdad8cd355321b9a360f87532c5e5a5f
shimizu-masahiro-macbook <uw0665@gmail.com>
2023-06-06 01:58:04 +0900

    6/6　検索機能の非同期化完了

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -6,6 +6,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
     <script src="/js/app.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
