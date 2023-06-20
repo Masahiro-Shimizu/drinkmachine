25dd2154f6647ee56ab0baef9bbcbcdf46c1d5f3
shimizu-masahiro-macbook <uw0665@gmail.com>
2023-06-21 03:31:06 +0900

    6/20

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -8,6 +8,7 @@
     <script src="/js/app.js" defer></script>
     <script src="{{ asset('js/search.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js"></script>
 </head>
 <body>
     <header>
